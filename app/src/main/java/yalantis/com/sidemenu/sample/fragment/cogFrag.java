2e7923ae845f30646c39de0e1552345beb3ec16c
togla <togla0213@naver.com>
2018-01-08 20:00:42 +0900

    9

---
+++ app/src/main/java/yalantis/com/sidemenu/sample/fragment/cogFrag.java
@@ -0,0 +1,22 @@
+package yalantis.com.sidemenu.sample.fragment;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import yalantis.com.sidemenu.sample.R;
+
+/**
+ * Created by adilsoomro on 8/19/16.
+ */
+public class cogFrag extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.layout_cog, container, false);
+    }
+}
---
+++ app/src/main/java/yalantis/com/sidemenu/sample/legacy/soju.java
@@ -0,0 +1,18 @@
+package yalantis.com.sidemenu.sample.legacy;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import yalantis.com.sidemenu.sample.R;
+
+/**
+ * @author Adil Soomro
+ *
+ */
+public class soju extends Activity {
+
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_soju);
+    }
+}
